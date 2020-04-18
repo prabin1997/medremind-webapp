8767b95bbebf87c02a61fac51674d19e16834823
prabin1997 <36660339+prabin1997@users.noreply.github.com>
2020-04-18 02:04:55 +0100

    remove dotenv

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 'use strict';
 
-require('dotenv-safe').load();
+//require('dotenv-safe').load();
 
 const cfg = {};
 
 // HTTP Port to run our web application
+cfg.port = process.env.PORT || 7802;
 
 // A random string that will help generate secure one-time passwords and
 // HTTP sessions
