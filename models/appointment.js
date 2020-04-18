6fb6f4cade916423a8b7c4213f7516f4da5cac16
prabin1997 <36660339+prabin1997@users.noreply.github.com>
2020-04-18 23:38:27 +0100

    twilio changes

--- models/appointment.js
+++ models/appointment.js
@@ -43,12 +43,12 @@ AppointmentSchema.statics.sendNotifications = function(callback) {
     * @param {array} appointments List of appointments.
     */
     function sendNotifications(appointments) {
-        const client = require('twilio')(cfg.twilioAccountSid, cfg.twilioAuthToken);
+        const client = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
         appointments.forEach(function(appointment) {
             // Create options to send the message
             const options = {
                 to: `+ ${appointment.phoneNumber}`,
-                from: cfg.twilioPhoneNumber,
+                from: TWILIO_PHONE_NUMBER,
                 /* eslint-disable max-len */
                 body: `Hi there. Just a reminder to take your ${appointment.name} medication.
                 Note: ${appointment.note} `,
