a9e74a6222efde257988fef05a8fdbd1c9385f9e
prabin1997 <36660339+prabin1997@users.noreply.github.com>
2020-04-19 17:38:05 +0100

    change env local

--- config.js
+++ config.js
@@ -4,24 +4,18 @@ require('dotenv').config();
 
 const cfg = {};
 
-// HTTP Port to run our web application
+// HTTP Port to run web application
 cfg.port = process.env.PORT || 7805;
 
 // A random string that will help generate secure one-time passwords and
 // HTTP sessions
 cfg.secret = process.env.APP_SECRET || 'keyboard cat';
 
-// Your Twilio account SID and auth token, both found at:
-// https://www.twilio.com/user/account
-//
-// A good practice is to store these string values as system environment
-// variables, and load them from there as we are doing below. Alternately,
-// you could hard code these values here as strings.
+
 cfg.twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
 cfg.twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
 
-// A Twilio number you control - choose one from:
-// Specify in E.164 format, e.g. "+16519998877"
+// A Twilio number you control 
 cfg.twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
 
 // MongoDB connection string - MONGO_URL is for local dev,
