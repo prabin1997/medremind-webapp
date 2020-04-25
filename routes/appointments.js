07032e29559cd53aec84de5baf09b8496b8a7773
prabin1997 <36660339+prabin1997@users.noreply.github.com>
2020-04-25 18:09:19 +0100

    update med list view

--- routes/appointments.js
+++ routes/appointments.js
@@ -41,7 +41,7 @@ router.get('/:id/edit', ensureAuthenticated, function(req, res, next) {
 router.get('/create', ensureAuthenticated, function(req, res, next) {
   const user = req.user.adminReq;
   Medication.find({ $and: [{ name: { $exists: true }},{ adminCode: { $eq: user }}]})
-  .select("name")
+  .select({ $project: {name: 1}, _id: 0})
   .exec()
   .then(function(result){
   res.render('appointments/create', { dropdownVals: result,
