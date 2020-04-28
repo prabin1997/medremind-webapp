d710103e3ee354e97670871c3d7c79aab78cdc03
prabin1997 <36660339+prabin1997@users.noreply.github.com>
2020-04-28 01:30:00 +0100

    update quantity

--- routes/appointments.js
+++ routes/appointments.js
@@ -138,7 +138,7 @@ router.get('/:id/fullMed', ensureAuthenticated, function(req, res, next) {
 
 router.post('/:id/fullMed/confirm', ensureAuthenticated, function(req, res, next) {
   const id = req.params.id;
-  const medName = req.params.name;
+  const medName = req.appointment.name;
   const user = req.user.adminReq;
   const msg = ["Diabetes is not terrible and there are many things you can do to prevent problems from diabetes, such as monitoring blood glucose, watching your diet, keeping fit, and taking pills regularly.",
   "Try brisk walking – a convenient, safe and cost-effective way of exercising! It’s good for your heart and will help control blood glucose.",
@@ -151,10 +151,10 @@ router.post('/:id/fullMed/confirm', ensureAuthenticated, function(req, res, next
   const randomMsg = msg[Math.floor(Math.random() * msg.length)];
   const successMsg = "Message of the day: ";
   
-  Medication.find({ $and: [{ name: { $eq: medName }},{ adminCode: { $eq: user }}, { quantity: { $exists: true }}]})
-  .update({$inc: { quantity: -1}}).then(function(){
   Appointment.update({_id: id}, {"$set":{"confirm": true}})
-    .then(function() {
+  .then(function() {
+  Medication.find({ $and: [{ name: { $eq: medName }},{ adminCode: { $eq: user }}, { quantity: { $exists: true }}]})
+  .update({"$inc": { "quantity": -1}}).then(function() {
       req.flash('success', "Sucessfully confirmed medication");
       req.flash('success', successMsg + randomMsg);
       res.redirect('/');
