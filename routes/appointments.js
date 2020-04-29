486544d77aeb97fabd0dea4cdd9a0fd302cef85c
prabin1997 <36660339+prabin1997@users.noreply.github.com>
2020-04-29 21:06:05 +0100

    update view med page

--- routes/appointments.js
+++ routes/appointments.js
@@ -127,12 +127,17 @@ router.post('/:id/delete', ensureAuthenticated, function(req, res, next) {
 });
 
 // full med details page 
-router.get('/:id/name/fullMed', ensureAuthenticated, function(req, res, next) {
+router.get('/:id/:name/fullMed', ensureAuthenticated, function(req, res, next) {
   const id = req.params.id;
+  const medname = req.params.name;
+  const user = req.user.adminReq;
+  Medication.find({ $and: [{ name: { $eq: medname }},{ adminCode: { $eq: user }}]})
+  .then(function(medications){
   Appointment.findOne({_id: id})
   .then(function(appointment) {
-      res.render('appointments/fullMed', {appointment: appointment});
+      res.render('appointments/fullMed', {appointment: appointment, medications: medications});
     });
+  });
 });
 
 router.post('/:id/:name/fullMed/confirm', ensureAuthenticated, function(req, res, next) {
