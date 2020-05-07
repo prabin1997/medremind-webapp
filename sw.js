698677fed5043827c22c2d147cdb5f2a6e6e6bf3
prabin1997 <36660339+prabin1997@users.noreply.github.com>
2020-05-07 21:40:26 +0100

    update offline

--- sw.js
+++ sw.js
@@ -1,14 +1,64 @@
+const CACHE_NAME = 'sw-cache-v1';
+const toCache = [
+  '/',
+  '/users/home',
+  '/users/about',
+  '/users/learn',
+  '/users/login',
+  '/users/contact',
+  '/public/css/style.css',
+  '/public/img/learn1.jpg',
+  '/public/img/learn2.jpg',
+  '/public/img/learn3.jpg',
+  '/public/img/med.png',
+  '/public/img/icons/favicon.ico',
+  '/public/js/main.js',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v48/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+  'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js',
+  'https://code.jquery.com/jquery-2.1.1.min.js'
+
+];
+
+
   // install event
-self.addEventListener('install', evt => {
-  console.log('service worker installed');
-});
+  self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          return cache.addAll(toCache)
+        })
+        .then(self.skipWaiting())
+    )
+  })
 
 // activate event
-self.addEventListener('activate', evt => {
-  console.log('service worker activated');
-});
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys()
+      .then((keyList) => {
+        return Promise.all(keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log('[ServiceWorker] Removing old cache', key)
+            return caches.delete(key)
+          }
+        }))
+      })
+      .then(() => self.clients.claim())
+  )
+})
+
 
 // fetch event
-self.addEventListener('fetch', evt => {
-  //console.log('fetch event', evt);
-});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => {
+        return caches.open(CACHE_NAME)
+          .then((cache) => {
+            return cache.match(event.request)
+          })
+      })
+  )
+})
